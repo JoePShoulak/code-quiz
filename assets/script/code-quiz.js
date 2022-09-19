43e920fde862e2f4ef409ded665379cbf4c9c7e7
Joe P Shoulak <joe@squarecompass.io>
2022-09-19 11:25:32 -0500

    Added more code questions, random selection thereof, and the beginnings of the timer and score

--- assets/script/code-quiz.js
+++ assets/script/code-quiz.js
@@ -1,10 +1,24 @@
 var codeQuiz = [
     {
-        question: "What does '.js' stand for?",
+        question: "What does 'js' stand for?",
         options: ["Just Sayin'",
             "Jumbled Syntax",
             "Janky System",
             "Javascript"],
         correct: "Javascript"
+    }, {
+        question: "What does 'css' stand for?",
+        options: ["Completely Stupid Syntax",
+            "Complicated Series of Statements",
+            "Cascading Style Sheets",
+            "Come See Seattle"],
+        correct: "Cascading Style Sheets"
+    }, {
+        question: "What does 'html' stand for?",
+        options: ["Hypertext Markup Language",
+            "Hugely Tedious Mess of a Language",
+            "Hot Tamales Marching Left",
+            "Here To Make Laughs"],
+        correct: "Cascading Style Sheets"
     },
 ]
--- assets/script/main.js
+++ assets/script/main.js
@@ -1,4 +1,22 @@
 var startButton = document.getElementById("start-button");
+var timerSpan = document.getElementById("timer");
+
+var choiceSpans = [
+    document.getElementById(`choice1`),
+    document.getElementById(`choice2`),
+    document.getElementById(`choice3`),
+    document.getElementById(`choice4`)
+]
+
+function randomFrom(items) {
+    return items[Math.floor(Math.random() * items.length)];
+}
+
+function adjustTimer() {
+    timer--;
+    timerSpan.innerHTML = `${timer}s`;
+    if (timer == 0) {clearInterval(t)};
+}
 
 function startQuiz() {
     // Ready to begin the quiz
@@ -6,11 +24,16 @@ function startQuiz() {
     document.getElementById("quizzlet").style.visibility = "visible";
  
     // Populate Quiz Options
-    document.getElementById("question").innerHTML = codeQuiz[0].question;
+    randomQ = randomFrom(codeQuiz);
+    document.getElementById("question").innerHTML = randomQ.question;
 
-    [1, 2, 3, 4].forEach(element => {
-        document.getElementById(`choice${element}`).innerHTML = codeQuiz[0].options[element-1];
+    [0, 1, 2, 3].forEach(i => {
+        choiceSpans[i].innerHTML = randomQ.options[i];
     });
+
+    timer = 60;
+
+    var t = setInterval(adjustTimer, 1000)
 
     // Receive User Input
 
